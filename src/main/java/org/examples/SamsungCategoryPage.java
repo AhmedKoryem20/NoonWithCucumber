124a5c208d445bfe2d58d338c57f4132344cd0c2
AhmedKoryem20 <koryem20@gmail.com>
2026-06-26 23:49:49 +0300

    This page for the main functionality to get the Samsung page

--- src/main/java/org/examples/SamsungCategoryPage.java
+++ src/main/java/org/examples/SamsungCategoryPage.java
@@ -9,9 +9,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class SamsungCategoryPage extends BasePage {
-    private By electronicsCategory = By.xpath("//*[@id=\"default-header-desktop\"]/div[1]/div/div/div/div/div/div[2]/ul/li[1]/a");
-    private By samsungCategory = By.xpath("//*[@id=\"default-header-desktop\"]/div[1]/div/div/div/div/div/div[2]/ul/li[1]/div/div/div[3]/div/ul/li[2]/a/img");
-    private By title = By.xpath("//*[@id=\"catalog-page-container\"]/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/h1");
+    private By electronicsCategory = By.xpath("//li[@data-qa='btn_main_menu_Electronics']");
+    private By samsungCategory = By.xpath("//img[@alt='/electronics-and-mobiles/samsung']");
+    private By title = By.xpath("//div[@id='catalog-page-container']//h1");
     public SamsungCategoryPage(WebDriver driver) {
         super(driver);
     }
