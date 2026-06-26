37394fd7a4103cafa8678f13b677be1a97cc2776
AhmedKoryem20 <koryem20@gmail.com>
2026-06-26 19:48:24 +0300

    Hooks handler where the setUp function "Before" and the tear down and function to get allure report "After" & "AfterMethod"

---
+++ src/test/java/Tests/Hooks/HooksHandler.java
@@ -0,0 +1,55 @@
+package Tests.Hooks;
+
+import Tests.BaseTest.BaseTest;
+import Tests.ConfigHandler;
+import Tests.Drivers.WebDriverFactory;
+import Tests.JSONFileManager;
+import Tests.ScreenShot;
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import io.qameta.allure.Allure;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.examples.SamsungCategoryPage;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.asserts.SoftAssert;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+
+public class HooksHandler extends BaseTest {
+    public Logger log = LogManager.getLogger(HooksHandler.class);
+
+    @Before
+    public void setUp() {
+        configHandler = new ConfigHandler("src/main/resources/config.properties");
+        jsonFileManager = new JSONFileManager("src/main/resources/Data.json");
+        driver = WebDriverFactory.getDriver(jsonFileManager.getValue("browser"));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        softAssert = new SoftAssert();
+        log.debug("Assertion Manager Initialized");
+        log.debug("config Manager Initialized");
+        samsungCategoryPage = new SamsungCategoryPage(driver);
+    }
+
+    @After
+    public void teardown() {
+        try {
+            Allure.addAttachment("log File", new FileInputStream("logs/application.log"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterStep
+    public void checkFail(Scenario scenario) throws IOException {
+        if (scenario.isFailed()){
+            File scrshoot = ScreenShot.takeScreenShot(driver, "screenshots/"+scenario.getName()+".png");
+            Allure.addAttachment("ScreenShot" , new FileInputStream(scrshoot));
+        }
+    }
+}
