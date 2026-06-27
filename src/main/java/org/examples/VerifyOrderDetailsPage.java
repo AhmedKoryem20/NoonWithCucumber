b36374448e2263421c575b37bb0c1dfac59e3ee7
AhmedKoryem20 <koryem20@gmail.com>
2026-06-28 01:29:42 +0300

    Verifying the order details main functions.

--- src/main/java/org/examples/VerifyOrderDetailsPage.java
+++ src/main/java/org/examples/VerifyOrderDetailsPage.java
@@ -2,40 +2,37 @@ package org.examples;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class VerifyOrderDetailsPage extends BasePage {
-    private By firstItemPrice = By.xpath("/html/body/div[3]/div/div/div[2]/div[1]/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div[2]/div[1]/div[2]/div[1]/div[1]/span/span");
-    private By secondItemPrice = By.xpath("/html/body/div[3]/div/div/div[2]/div[1]/div/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[1]/div[2]/div[1]/div[1]/span/span");
-    private By thirdItemPrice = By.xpath("/html/body/div[3]/div/div/div[2]/div[1]/div/div[2]/div/div[2]/div/div/div/div/div[2]/div[1]/div[2]/div[1]/div/span/span");
-    private By firstItemName = By.xpath("/html/body/div[3]/div/div/div[2]/div[1]/div/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[1]/div[1]/a/div/span/semibold");
-    private By secondItemName = By.xpath("/html/body/div[3]/div/div/div[2]/div[1]/div/div[2]/div/div[2]/div/div/div/div/div[2]/div[1]/div[1]/a/div/span/semibold");
-    private By thirdItemName = By.xpath("/html/body/div[3]/div/div/div[2]/div[1]/div/div[2]/div/div[2]/div/div/div/div/div[2]/div[1]/div[1]/a/div/span/semibold");
-    private By totalPrice = By.xpath("/html/body/div[3]/div/div/div[2]/div[2]/div/div[1]/div[1]/div/div/div/div[2]/div[3]/div[2]/medium/b");
+
+    private By totalPrice = By.xpath("//*[normalize-space()='Total']/following::b[1]");
+    private By productsPrice = By.xpath("//span[@style='font-weight: 700;']");
     public  VerifyOrderDetailsPage(WebDriver driver) {
         super(driver);
     }
 
-    public boolean getFirstItemPrice(String productPrice) {
-        return findElement(firstItemPrice).isDisplayed();
+    public double getTotalPrice() {
+
+        String invoiceText= driver.findElement(totalPrice).getText().replace("," ,"")
+                .replace("EGP","").trim();
+        return Double.parseDouble(invoiceText);
     }
-    public  boolean getSecondItemPrice(String productPrice) {
-        return findElement(secondItemPrice).isDisplayed();
+
+    public double getTheSumOfProducts(){
+        List<WebElement> prices =driver.findElements(productsPrice);
+        double priceSum = 0.0;
+        for (WebElement price : prices){
+
+            String priceText = price.getText().replace(",","").trim();
+            priceSum += Double.parseDouble(priceText);
+        }
+        return priceSum;
     }
-    public  boolean getThirdItemPrice(String productPrice) {
-        return findElement(thirdItemPrice).isDisplayed();
-    }
-    public boolean getFirstItemName(String productName) {
-        return findElement(firstItemName).isDisplayed();
-    }
-    public  boolean getSecondItemName(String productName) {
-        return findElement(secondItemName).isDisplayed();
-    }
-    public boolean getThirdItemName(String productName) {
-        return findElement(thirdItemName).isDisplayed();
-    }
-    public boolean getTotalPrice(String totalPrice1) {
-        return findElement(totalPrice).isDisplayed();
-    }
+
+
 
 
 
