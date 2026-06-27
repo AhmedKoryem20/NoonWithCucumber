d35a607aabd4ef8bce7f3eb19631d581f059ac09
AhmedKoryem20 <koryem20@gmail.com>
2026-06-28 01:32:35 +0300

    page: contains functions to go to cart.

--- src/main/java/org/examples/GoToCartPage.java
+++ src/main/java/org/examples/GoToCartPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 
 public class GoToCartPage extends BasePage{
     private By cartIcon = By.xpath("//*[@id=\"default-header-desktop\"]/header/div[2]/a[3]");
-    private By OrderSummary = By.xpath("/html/body/div[3]/div/div/div[2]/div[2]/div/div[1]/div[1]/div/div/div/div[1]/div[1]");
+    private By OrderSummary = By.xpath("//div[normalize-space()='Order Summary']");
     public GoToCartPage(WebDriver driver) {
         super(driver);
     }
