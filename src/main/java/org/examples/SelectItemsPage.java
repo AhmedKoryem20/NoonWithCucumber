f4dedf687ea01c540ef09f15379baf4a3f82b1ce
AhmedKoryem20 <koryem20@gmail.com>
2026-06-29 05:55:04 +0300

    Page: Added clickRemoveItem method and removeItem method and modified clickAddToCart method by adding wait to ensure safe click.

--- src/main/java/org/examples/SelectItemsPage.java
+++ src/main/java/org/examples/SelectItemsPage.java
@@ -1,9 +1,10 @@
 package org.examples;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,9 +19,9 @@ public class SelectItemsPage extends BasePage{
 
 
     public void clickAddToCart(String productKeyword) {
-        driver.findElement(By.xpath(
-                "//div[contains(@data-qa,'" + productKeyword + "')]//button[.//img[@alt='add-to-cart']]"
-        )).click();
+        WebDriverWait wait = new WebDriverWait(driver , Duration.ofSeconds(20));
+        WebElement addToCartBtn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(@data-qa,'" + productKeyword + "')]//button[.//img[@alt='add-to-cart']]")));
+        addToCartBtn.click();
     }
 
     public void pickItems(String item1 , String item2, String item3) {
@@ -29,6 +30,26 @@ public class SelectItemsPage extends BasePage{
         clickAddToCart(item2);
         clickAddToCart(item3);
 
+    }
+    public void clickRemoveItem(String productKeyword) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+       WebElement removeFromCartBtn =  wait.until(ExpectedConditions.elementToBeClickable(By.xpath(
+               "//div[contains(@class,'CartItemDesktop')][.//div[contains(@class,'__title')]//*[contains(text(),'"
+                       + productKeyword + "')]]//button[@data-qa='cart-remove_item']")));
+       removeFromCartBtn.click();
+    }
+
+    public void removeItem(String item) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        By productCard = By.xpath(
+                "//div[contains(@class,'CartItemDesktop')]" +
+                        "[.//div[contains(@class,'__title')]//*[contains(text(),'" + item + "')]]"
+        );
+
+        clickRemoveItem(item);
+
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(productCard));
     }
 
 
