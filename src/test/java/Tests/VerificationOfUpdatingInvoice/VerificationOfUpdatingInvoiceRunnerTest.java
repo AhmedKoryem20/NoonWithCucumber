e65b22eb4ff854a7421e359e071c4626eff373c1
AhmedKoryem20 <koryem20@gmail.com>
2026-06-29 05:48:28 +0300

    feat: Verify that the total invoice is updated with the proper amount of money after removing items from the cart.

---
+++ src/test/java/Tests/VerificationOfUpdatingInvoice/VerificationOfUpdatingInvoiceRunnerTest.java
@@ -0,0 +1,14 @@
+package Tests.VerificationOfUpdatingInvoice;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions (features = "src/test/java/Tests/VerificationOfUpdatingInvoice",
+        glue = {"Tests"},
+        plugin = {"pretty" ,
+                "html:target/cucumber-reports.html",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"})
+
+
+public class VerificationOfUpdatingInvoiceRunnerTest extends AbstractTestNGCucumberTests {
+}
