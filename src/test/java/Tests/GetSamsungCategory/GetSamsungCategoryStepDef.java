dd742a6f17529dbca25acea4fea9f395e1546e38
AhmedKoryem20 <koryem20@gmail.com>
2026-06-29 05:50:55 +0300

    feat: Choosing Samsung category from the electronics list.

--- src/test/java/Tests/GetSamsungCategory/GetSamsungCategoryStepDef.java
+++ src/test/java/Tests/GetSamsungCategory/GetSamsungCategoryStepDef.java
@@ -6,7 +6,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+
 
 
 
