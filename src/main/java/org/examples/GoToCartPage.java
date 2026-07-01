e257310939f8c25658db621ff06cff69996b2210
AhmedKoryem20 <koryem20@gmail.com>
2026-07-01 07:13:00 +0300

    page: Added getSelectedTitlesInCart method.

--- src/main/java/org/examples/GoToCartPage.java
+++ src/main/java/org/examples/GoToCartPage.java
@@ -2,6 +2,10 @@ package org.examples;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GoToCartPage extends BasePage{
     private By cartIcon = By.xpath("//*[@id=\"default-header-desktop\"]/header/div[2]/a[3]");
@@ -17,4 +21,18 @@ public class GoToCartPage extends BasePage{
     public String isCartDisplayed() {
         return findElement(OrderSummary).getText();
     }
+
+    public List<String> getSelectedTitlesInCart() {
+        List<WebElement> titleElement;
+        titleElement = driver.findElements(By.xpath(
+                "//a[contains(@class,'titleLink')]//div[contains(@class,'title')]"));
+
+        List<String> productTitlesInCart = new ArrayList<>();
+        for (WebElement title : titleElement) {
+            productTitlesInCart.add(normalizeText(title.getText()));
+            System.out.printf("Title: %s\n", title.getText().trim());
+        }
+        return productTitlesInCart;
+    }
 }
+
